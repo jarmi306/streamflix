6d42654c1b26f8ae23de1b3c112de0e6e37d1a88
jarmi306 <miisme306@gmail.com>
2025-12-29 21:50:48 +0600

    Fix: Enable Tailwind styling

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        netflix: {
+          red: '#E50914',
+          black: '#141414',
+          dark: '#09090b',
+        }
+      },
+      fontFamily: {
+        sans: ['Inter', 'ui-sans-serif', 'system-ui', '-apple-system', 'BlinkMacSystemFont', 'Segoe UI', 'Roboto', 'Helvetica Neue', 'Arial', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
